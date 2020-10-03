46d8e7654d5b562bd2cce0db69ad6d18d9f8b6d5
ethanvar <ethanvar6@gmail.com>
2020-10-03 17:38:39 -0400

    as

--- MovieProject.js
+++ MovieProject.js
@@ -9,3 +9,19 @@ document.getElementById("myForm").style.display = "none";
 function newAccount() {
     location.href = "SignUp.html";
 }
+
+var movieCounter = 0;
+sugMovies();
+
+function sugMovies() {
+  var i;
+  var slides = document.getElementsByClassName("sugMovieSlide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  movieCounter++;
+  if (movieCounter > slides.length) {movieCounter = 1}    
+
+  slides[movieCounter-1].style.display = "block";  
+  setTimeout(sugMovies, 2000); // Change image every 2 seconds
+}
