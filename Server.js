b9d0000469b4eef2c899b81fcd4af5007ff9cf66
ethanvar <ethanvar6@gmail.com>
2020-10-15 19:09:35 -0400

    sad

--- Server.js
+++ Server.js
@@ -1,9 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+var http = require('http');
 
 const PORT=8080; 
 
+app.use(express.static(path.join(__i-see-flicks, 'public')));
+
 fs.readFile('./Movie_Project.html', function (err, html) {
+
 
     if (err) throw err;    
 
@@ -11,5 +15,5 @@ fs.readFile('./Movie_Project.html', function (err, html) {
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    }).listen(PORT);
+    }).listen(PORT); 
 });
