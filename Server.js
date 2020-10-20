7cf95d935aa3b43639cc6aced3a127bb30a70fe8
ethanvar <ethanvar6@gmail.com>
2020-10-20 16:25:17 -0400

    asa

--- Server.js
+++ Server.js
@@ -1,42 +1,103 @@
-/*var http = require('http');
-var fs = require('fs');
-var http = require('http');
+/*
+const http = require('http')
+const fs = require('fs')
 
-const PORT=8080; 
+let server = http.createServer(function (request, response) {
+  console.log("URL: " + request.url); 
 
-app.use(express.static(path.join(__i-see-flicks, 'public')));
+  if (request.method === 'GET') {
+    if (request.url === '/' || request.url ===  '/Movie_Project.html' ) {
+        fs.readFile('index.html', function(err, data) {
+          response.statusCode = 200;
+          response.setHeader("Content-Type", 'text/html')
+          response.end(data)
 
-fs.readFile('./Movie_Project.html', function (err, html) {
-
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT); 
-});*/
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-
-http.createServer(function (request, response) {
-    console.log('request starting...');
-
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './Movie_Project.html';
-
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
+        })
     }
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+    else if (request.url === '/' || request.url ==='/MovieProject.js') {
+        fs.readFile('MovieProject.js', function(err, data){
+            response.statusCode = 200;
+            response.setHeader("Content-Type", 'application/javascript')
+            response.end(data);  
+        })
+    }
+    
+    else if (request.url === '/' || request.url ==='/Movie_Project.css') {
+        fs.readFile('Movie_Project.css', function(err, data){
+            response.statusCode = 200;
+            response.setHeader("Content-Type", 'style/css')
+            response.end(data);  
+        })
+    } 
+ 
+  }
+
+})
+
+server.listen(3000);
+
+console.log('Server running at http://127.0.0.1:3000');*/
+
+//1.1
+const http = require('http');
+
+//2.1
+const fs = require("fs");
+
+//1.2
+let server = http.createServer(function (request, response) {
+	//3.1
+	console.log("URL: " + request.url);
+	//3.2
+	if (request.method === "GET"){
+		//3.3
+		if (request.url === "/" || request.url === "/Movie_Project.html"){
+			//3.4
+			fs.readFile("Movie_Project.html", function(err, data){
+				//4.1
+				if(err){
+					response.statusCode = 500;
+					response.end("Error reading file.");
+					return;
+				}
+				//4.2
+				response.statusCode = 200;
+				response.setHeader("Content-Type", "text/html");
+				response.end(data);
+			});
+		}
+		//4.3
+		else if (request.url === "/MovieProject.js"){
+			fs.readFile("MovieProject.js", function(err, data){
+				if(err){
+					response.statusCode = 500;
+					response.end("Error reading file.");
+					return;
+				}
+				response.statusCode = 200;
+				response.setHeader("Content-Type", "application/javascript");
+				response.end(data);
+			});
+		}		
+
+		//4.4
+		/*else if (request.url === "/Movie_Project.css"){
+			fs.readFile("Movie_Project.css", function(err, data){
+				if(err){
+					response.statusCode = 500;
+					response.end("Error reading image.");
+					return;
+				}
+				response.statusCode = 200;
+				response.setHeader("Content-Type", "style/css");
+				response.end(data);
+			});
+		}*/
+		//5.2
+	}
+	//4.5
+});
+
+//1.3
+server.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
