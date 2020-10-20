aebaf102f1f18b150521f5293423807e68886944
ethanvar <ethanvar6@gmail.com>
2020-10-20 18:56:07 -0400

    n

--- Server.js
+++ Server.js
@@ -44,12 +44,32 @@ const http = require('http');
 //2.1
 const fs = require("fs");
 
+/*const express = require('express')
+
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('i-see-flicks'));
+*/
 //1.2
 let server = http.createServer(function (request, response) {
 	//3.1
 	console.log("URL: " + request.url);
-	//3.2
+    //3.2
+    
+
+    function css(request, response) {
+        if (request.url === '/Movie_Project.css') {
+          response.writeHead(200, {'Content-type' : 'text/css'});
+          var fileContents = fs.readFileSync('./Movie_Project.css', {encoding: 'utf8'});
+          response.write(fileContents);
+        }
+      }
+
 	if (request.method === "GET"){
+
+        
 		//3.3
 		if (request.url === "/" || request.url === "/Movie_Project.html"){
 			//3.4
@@ -66,7 +86,7 @@ let server = http.createServer(function (request, response) {
 				response.end(data);
 			});
 		}
-		//4.3
+        //4.3
 		else if (request.url === "/MovieProject.js"){
 			fs.readFile("MovieProject.js", function(err, data){
 				if(err){
@@ -81,7 +101,7 @@ let server = http.createServer(function (request, response) {
 		}		
 
 		//4.4
-		/*else if (request.url === "/Movie_Project.css"){
+		else if (request.url === "/Movie_Project.css"){
 			fs.readFile("Movie_Project.css", function(err, data){
 				if(err){
 					response.statusCode = 500;
@@ -92,12 +112,11 @@ let server = http.createServer(function (request, response) {
 				response.setHeader("Content-Type", "style/css");
 				response.end(data);
 			});
-		}*/
-		//5.2
+		}
+        //5.2
 	}
 	//4.5
 });
-
 //1.3
 server.listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
