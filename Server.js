c8ec08f17a5ad92562748fe4c62c6f5433e94b96
ethanvar <ethanvar6@gmail.com>
2020-11-08 13:16:57 -0500

    dfs

--- Server.js
+++ Server.js
@@ -1,16 +1,18 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 var path = require('path');
 
 
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "i-see-flicks"));
+/*app.set("views", path.join(__dirname, "i-see-flicks"));
 app.use(express.static(__dirname));
 // viewed at http://localhost:3000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,'Movie_Project.html'));
-});
+}); */
+
+app.get("/", (req, res, next)=> { res.render("Movie_Project"); });
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000');
