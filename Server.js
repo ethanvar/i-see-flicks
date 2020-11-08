b18ff52a7f635ef6e30a84cd1891e2427fa011f8
ethanvar <ethanvar6@gmail.com>
2020-11-08 00:22:57 -0500

    gh

--- Server.js
+++ Server.js
@@ -3,13 +3,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "i-see-flicks"));
 app.use(express.static(__dirname));
 // viewed at http://localhost:3000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,'Movie_Project.html'));
 });
-
-
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000');
